fd423fbd3cd478d6ee09f03092b80fade51ab342
Anuj <chronal@chronal.space>
2025-07-27 10:34:18 +1000

    Adds refs to report in comments in parser file

--- src/parser/parser.lisp
+++ src/parser/parser.lisp
@@ -1,3 +1,44 @@
 (in-package algol60/parser)
 
+(define-constant +reserved-functions+
+    '("abs"
+      "sign"
+      "sqrt"
+      "sin"
+      "cos"
+      "arctan"
+      "ln"
+      "log"
+      "exp"
+      "entier") :test 'equal)
+
+;;; Main function driving parsing
 (defun parse (tokens))
+
+;;; Variables (§3.1)
+
+;;; Functions & procedures (§3.2)
+
+;;; Arithmetic expressions (§3.3)
+
+;;; Boolean expressions (§3.3)
+
+;;; Compound statements & blocks (§4.1)
+
+;;; Assignment statements (§4.2)
+
+;;; Go statements (§4.3)
+
+;;; Conditional statements (§4.5)
+
+;;; For statements (§4.6)
+
+;;; Procedure statements (§4.7)
+
+;;; Type declarations (§5.1)
+
+;;; Array declarations (§5.2)
+
+;;; Switch declarations (§5.3)
+
+;;; Procedure declarations (§5.4)
